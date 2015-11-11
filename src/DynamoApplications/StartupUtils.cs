96787eb6b6fbc94e1c08a0fe427acb2ae54cf25f
racel <racel.williams@autodesk.com>
2015-11-11 09:19:37 -0500

    Running Sandbox with Customizer
    Running Sandbox with Customizer

--- src/DynamoApplications/StartupUtils.cs
+++ src/DynamoApplications/StartupUtils.cs
@@ -14,6 +14,7 @@ using System.Diagnostics;
 using System.Threading;
 using System.Globalization;
 using NDesk.Options;
+using Greg.AuthProviders;
 
 
 namespace Dynamo.Applications
@@ -187,7 +188,8 @@ namespace Dynamo.Applications
 
             config.UpdateManager = CLImode ? null : InitializeUpdateManager();
             config.StartInTestMode = CLImode ? true : false;
-            config.PathResolver = CLImode ? new CLIPathResolver(preloaderLocation) as IPathResolver : new SandboxPathResolver(preloaderLocation) as IPathResolver ; 
+            config.PathResolver = CLImode ? new CLIPathResolver(preloaderLocation) as IPathResolver : new SandboxPathResolver(preloaderLocation) as IPathResolver ;
+            config.AuthProvider = new OxygenProvider("https://accounts-staging.autodesk.com/");
 
             var model = DynamoModel.Start(config);
             return model;
